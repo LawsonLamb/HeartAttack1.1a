d74dcad0e3cfadb4db57052bb343faa0230827f3
Lawson Lamb <lawlzlamb@gmail.com>
2016-02-08 12:56:45 -0500

    Camera/Test Level implemented

---
+++ Assets/_Scripts/CameraTrigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTrigger : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D col){
+
+		if (col.gameObject.tag == "Player"){
+
+			GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
+			//Vector2.Lerp(camera.transform.position,this.transform.position,Time.deltaTime);
+
+			camera.transform.position =	new Vector3(this.transform.position.x,this.transform.position.y,camera.transform.position.z);
+
+		}
+	}
+
+
+
+
+
+
+
+}
