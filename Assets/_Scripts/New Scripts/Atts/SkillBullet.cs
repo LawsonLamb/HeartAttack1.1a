37be37082b50a1b981c0663d71f5d2af747ee482
LawsonLamb <lawlzlamb@gmail.com>
2016-04-20 12:23:08 -0400

    fixed Conflict with SkillBullet

--- Assets/_Scripts/New Scripts/Atts/SkillBullet.cs
+++ Assets/_Scripts/New Scripts/Atts/SkillBullet.cs
@@ -61,15 +61,15 @@ public class SkillBullet : MonoBehaviour {
 			ani.SetTrigger ("ObjectHit");
 		}
 	}
-<<<<<<< HEAD
+
 
 	public void Splat () {
 		Destroy (gameObject);
 	}
-=======
+
     void OnDestroy()
     {
         Instantiate(ImpactEffect, this.transform.position, Quaternion.identity);
     }
->>>>>>> origin/master
+
 }
